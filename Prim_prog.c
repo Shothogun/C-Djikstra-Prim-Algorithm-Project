f2c7ad1ce60b138b70fc9537d28925f46ffc8089
Shothogun <33355906+Shothogun@users.noreply.github.com>
2017-11-10 10:39:05 -0200

    Update Prim_prog.c

--- Prim_prog.c
+++ Prim_prog.c
@@ -1,31 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include "Prim_prog.h"
-#include "Graph.h"
+#ifndef GRAPH_H
+#define GRAPH_H
 
-void MST_Prim(Graph* G, unsigned int r){
-	Init_sng_src(G);
-	G->Vertex[r-1].key = 0;
-	Type_Heap *Q = MPQ_creator(G, r);
-	while (Q->size != 0)
-	{
-		int i = Extract_min(Q);
-		Min_heapify(Q,0);
-		G->B_queue[i-1] = 0;
-		List *u = &G->Vertex[i-1];
-		for(List_Node *v = u->head->next; v != NULL; v = v->next)
-		{
-			if( G->B_queue[v->id-1] == 1 &&
-				v->WT < G->Vertex[v->id-1].key)
-			{
-				G->Vertex[v->id-1].pi = i;
-				G->Vertex[v->id-1].key = v->WT;
-				Decrease_key(Q, Q->position[v->id-1], v->WT);
-				Min_heapify(Q,0);
-			}
-		}
-	}
+// Graph
+typedef struct List_Node *Adj;
 
-	Free_queue(Q);
-} 
+//Adj vertice information
+typedef struct List_Node
+{
+	Adj next;
+	//Vertex identification
+	unsigned int id;
+	//Edge's weight
+	double WT;
+} List_Node;
+
+//List of adjacents vertices from a ith vertex
+typedef struct List
+{
+	// Node head
+	Adj head;
+	// Last node
+	Adj last;
+	//Vertex's key	
+	double key;
+	//Vertex's father
+	unsigned int pi;	
+} List;
+
+typedef struct Graph
+{	
+	//Graph's size
+	unsigned int size;		
+	//Graph's Vertices(array)		
+	List* Vertex;
+	// Vertex's belongs to queue indicator(array)
+	short* B_queue; 
+} Graph;
+
+//-------------------------------------------------------
+
+// Min prior queue 
+typedef struct Heap_node *Pointer;
+
+typedef struct Heap_node
+{
+	//Vertex identification
+	unsigned int id;
+	//Edge's weight
+	double key;
+} Heap_node;
+
+typedef struct Type_Heap
+{
+	// Vertices array(array)
+	Heap_node* MPQ;
+	// Size of queue
+	unsigned int size;
+	// Vertex position in queue(array)
+	unsigned int* position;
+} Type_Heap;
+
+
+Heap_node* Parent(Type_Heap* A, unsigned int i);
+Heap_node* Left(Type_Heap* A, unsigned int i);
+Heap_node* Right(Type_Heap* A, unsigned int i);
+void swap(Type_Heap* Q, Heap_node* x, Heap_node* y);
+void Min_heapify(Type_Heap* A, unsigned int i);
+int Extract_min(Type_Heap* A);
+void Decrease_key(Type_Heap *Q, int i, double new_val);
+void Init_sng_src(Graph* G);
+Graph* Init_graph(unsigned int size);
+void Graph_updater(Graph* G, 
+				   unsigned int v1,
+				   unsigned int v2,
+				   double w);
+Type_Heap* MPQ_creator(Graph* G, unsigned int r);
+void Free_graph(Graph *G);
+void Free_queue(Type_Heap *Q);
+
+#endif
