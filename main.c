c313415b42bc5647104013af40b477c40723d323
Shothogun <33355906+Shothogun@users.noreply.github.com>
2017-11-22 23:16:57 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Dijkstra_prog.h"
 #include "Prim_prog.h"
 #include "Graph.h"
 #include "IO.h"
-
+	
 int main(int argc, char* argv[]){
 	int flag;
 
@@ -12,7 +13,14 @@ int main(int argc, char* argv[]){
 
 	unsigned int size;
 	
-	unsigned int r = 1;
+	unsigned int r = atoi(argv[2]);
+
+	unsigned int end = 0;
+
+	if ( argc > 3)
+	{
+		end = atoi(argv[3]);
+	}
 
 	scanf("%u%*c", &size);
 
@@ -20,11 +28,47 @@ int main(int argc, char* argv[]){
 
 	flag = read(G);
 
-	MST_Prim(G, r);
-	
-	print_MST(G, r, &weight);
-	printf("Peso Total:%lf\n", weight);
+	if(strcmp(argv[1], "Dijkstra") == 0)
+	{
+		if(flag == 1){
+			printf("Nao é possivel processar arestas negativas\n");
+			return 0;
+		}
+		Dijkstra(G, r);
+		print_distance(G, end);
+		printf("Distancia:%lf\n", G->Vertex[end-1].key);
+	}
 
+	else if (strcmp(argv[1], "MST") == 0)
+	{
+		MST_Prim(G, r);
+		print_MST(G, r, &weight);
+		printf("Peso Total:%lf\n", weight);
+				
+	}
+
+	else if (strcmp(argv[1], "Freq") == 0)
+	{
+
+	}
+
+	else
+	{
+		printf("ERROR");
+	}
+
+	
+
+	/*
+	for(unsigned int i=0; i<size; i++)
+	{
+		for(List_Node *v = G->Vertex[i].head->next ; v != NULL; v = v->next)
+		{
+			printf("Vertice (%u) adj (%u) w (%lf)\n", i+1, v->id, v->WT);
+		}
+		printf("parent (%u)\n", G->Vertex[i].pi);
+	}
+	*/
 
 	Free_graph(G);
 
