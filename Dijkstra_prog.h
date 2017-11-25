6318696f4f42b24243ea9e7800e65542317f0f12
Shothogun <33355906+Shothogun@users.noreply.github.com>
2017-11-25 12:42:04 -0200

    Update Dijkstra_prog.h

--- Dijkstra_prog.h
+++ Dijkstra_prog.h
@@ -1,11 +1,8 @@
-#ifndef Dijkstra_PROG_Hh
+#ifndef Dijkstra_PROG_H
 #define Dijkstra_PROG_H
-#include "Graph.h"
 
 void Relax(Graph *G, List_Node *v, unsigned int i);
 
 void Dijkstra(Graph *G, unsigned int r);
-
-
 
 #endif
