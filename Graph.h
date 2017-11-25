c616053ab621de9c17d07010c5b8a6b0d0b8b1c8
Shothogun <33355906+Shothogun@users.noreply.github.com>
2017-11-25 12:41:15 -0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -78,21 +78,42 @@ typedef struct Type_Heap
 
 // Graph's subroutines
 
+// arguments:()
 Heap_node* Parent(Type_Heap* A, unsigned int i);
 Heap_node* Left(Type_Heap* A, unsigned int i);
 Heap_node* Right(Type_Heap* A, unsigned int i);
+
+// arguments:(queue, nodes swapped)
 void swap(Type_Heap* Q, Heap_node* x, Heap_node* y);
+
+// arguments:(queue, vertices position in queue)
 void Min_heapify(Type_Heap* A, unsigned int i);
+
+// arguments:(queue)
 int Extract_min(Type_Heap* A);
+
+// arguments:(queue, vertices position in queue, new weight value)
 void Decrease_key(Type_Heap *Q, int i, double new_val);
+
+// arguments:(graph)
 void Init_sng_src(Graph* G);
+
+// arguments:(graph's size)
 Graph* Init_graph(unsigned int size);
+
+// arguments:(graph, vertices id, and edge's weight)
 void Graph_updater(Graph* G, 
 				   unsigned int v1,
 				   unsigned int v2,
 				   double w);
+
+// arguments:(graph, source's vertices id)
 Type_Heap* MPQ_creator(Graph* G, unsigned int r);
+
+// arguments:(graph)
 void Free_graph(Graph *G);
+
+// arguments:(queue)
 void Free_queue(Type_Heap *Q);
 
 #endif
